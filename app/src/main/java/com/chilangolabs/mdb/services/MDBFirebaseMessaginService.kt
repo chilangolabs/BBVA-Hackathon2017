9ec95b590ea034335c29045d794fcb660dcf9820
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-10-29 16:30:54 -0600

    fix firebase messagin Service

--- app/src/main/java/com/chilangolabs/mdb/services/MDBFirebaseMessaginService.kt
+++ app/src/main/java/com/chilangolabs/mdb/services/MDBFirebaseMessaginService.kt
@@ -6,8 +6,8 @@ import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import android.support.v4.app.NotificationCompat
-import com.chilangolabs.mdb.CardsActivity
 import com.chilangolabs.mdb.R
+import com.chilangolabs.mdb.TransferActivity
 import com.chilangolabs.mdb.utils.loge
 import com.chilangolabs.mdb.utils.logi
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -32,7 +32,7 @@ class MDBFirebaseMessaginService : FirebaseMessagingService() {
     }
 
     private fun sendNotification(messageBody: RemoteMessage) {
-        val intent = Intent(this, CardsActivity::class.java)
+        val intent = Intent(this, TransferActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         val pendingIntent = PendingIntent.getActivity(this, System.currentTimeMillis().toInt(), intent, PendingIntent.FLAG_ONE_SHOT)
 
